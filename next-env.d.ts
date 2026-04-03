1771d42de796ef09699b89676d920ad8ee21f934
MadoPanda <shivrajnikam2104@gmail.com>
2026-04-03 17:07:55 +0530

    Final cleanup of next-env.d.ts markers

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-/// <reference path="./.next/types/routes.d.ts" />
 
 // NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-=======
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-/// <reference path="./.next/types/routes.d.ts" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
->>>>>>> 955ef3d569783984a4aa43e38b9bc016082cb108
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
